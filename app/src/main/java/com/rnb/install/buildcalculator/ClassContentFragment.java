25196ab849fb49dbf15bdfe289950f16fa2d3935
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2017-04-04 01:09:55 -0400

    finished most of the coding for viewpager to function , still need to add content to viewpager which requires further discussion

--- app/src/main/java/com/rnb/install/buildcalculator/ClassContentFragment.java
+++ app/src/main/java/com/rnb/install/buildcalculator/ClassContentFragment.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 /**
@@ -67,10 +70,145 @@ public class ClassContentFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_class_content, container, false);
-        //fab.setImageResource(R.drawable.ic_email_black_24dp);
+        //creating the viewpager
         sectionPagerAdapter = new SectionPagerAdapter(getChildFragmentManager());
+        viewPager = (ViewPager) view.findViewById(R.id.classcontent);
+        viewPager.setAdapter(sectionPagerAdapter);
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        ImageButton leftButton = (ImageButton) view.findViewById(R.id.left);
+        ImageButton rightButton = (ImageButton) view.findViewById(R.id.right);
+
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int location = viewPager.getCurrentItem();
+                if(location > 0){
+                    location--;
+                    viewPager.setCurrentItem(location);
+                }
+                else if(location == 0){
+                    viewPager.setCurrentItem(viewPager.getChildCount()+2);
+                }
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int location = viewPager.getCurrentItem();
+                location++;
+                if(location >= viewPager.getChildCount()+2){
+                    viewPager.setCurrentItem(0);
+                }
+                else{
+                    viewPager.setCurrentItem(location);
+                }
+            }
+        });
 
         return view;
+    }
+    public class SectionPagerAdapter extends FragmentPagerAdapter {
+        public SectionPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+        public Fragment getItem(int position){
+            //adding content to the viewpager
+            switch(position){
+                case 0:
+                    return ClassFragment.newInstance("Rule #1",R.drawable.prohibition, "DO NOT sacrifice your study for the club , club activities should only be taken during your free time.");
+                case 1:
+                    return ClassFragment.newInstance("Rule #2",R.drawable.handshake, "Play and carries out activities in a way so that everyone have fun .This involves knowing and following the rules, Staying focused on the game, playing at an appropriate volume level, showing good sportsmanship and making consensus decisions on the games that are played.\n");
+                case 2:
+                    return ClassFragment.newInstance("Rule #3",R.drawable.respect, "Treat the game materials with the utmost respect . This means that treating club's properties like consoles , peripherals, tools ,etc carefully.If you played or handled any club's equipments,make sure to clean up before leaving ");
+                case 3:
+                    return ClassFragment.newInstance("Rule #4",R.drawable.food, "Food and drinks are not allowed inside the clubroom due to potential risk of damaging equipments");
+                case 4:
+                    return ClassFragment.newInstance("Rule #5",R.drawable.money, "If you break it , you pay for it . Meaning any serious damage caused to equipment will result in the person having to pay back to the club so we can get a replacement");
+                default:
+                    return ClassFragment.newInstance("Rule #1",R.drawable.prohibition, "DO NOT sacrifice your study for the club , club activities should only be taken during your free time.");
+            }
+        }
+        public int getCount(){
+            return 5;
+        }
+
+    }
+
+    public class DepthPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.setAlpha(1);
+                view.setTranslationX(0);
+                view.setScaleX(1);
+                view.setScaleY(1);
+
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.setAlpha(1 - position);
+
+                // Counteract the default slide transition
+                view.setTranslationX(pageWidth * -position);
+
+                // Scale the page down (between MIN_SCALE and 1)
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
