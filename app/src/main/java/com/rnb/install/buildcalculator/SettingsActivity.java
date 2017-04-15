f470883bcb73695a66b861b081a00425aa29c3d1
blaze blackbird <blaze.blackbird01@stclairconnect.ca>
2017-04-14 21:27:49 -0400

    Added shared pereferences for the settings

---
+++ app/src/main/java/com/rnb/install/buildcalculator/SettingsActivity.java
@@ -0,0 +1,27 @@
+package com.rnb.install.buildcalculator;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import java.util.prefs.PreferenceChangeEvent;
+
+/**
+ * Created by blaze on 2017-04-14.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new BuildPrefFragment()).commit();
+    }
+
+    public static class BuildPrefFragment extends PreferenceFragment {
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
